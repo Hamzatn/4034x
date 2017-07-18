90d7d97906c5b9ae4e358e61698208415a4d01ec
Hamzatn <hamzahtn@gmail.com>
2017-07-19 02:58:52 +0300

    Create system_prop.mk

---
+++ system_prop.mk
@@ -0,0 +1,47 @@
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib/libbrcm_ril.so \
+    ro.telephony.ril_class=tclBCMRIL \
+    persist.radio.multisim.config=dsds \
+    ro.telephony.call_ring.multiple=0 \
+    ro.telephony.call_ring=0
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=131072 \
+    debug.hwui.render_dirty_regions=false \
+    ro.zygote.disable_gl_preload=1 \
+    ro.sf.lcd_density=240
+
+# Misc
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.brcm.log=none \
+    persist.brcm.ap_crash=none \
+    persist.brcm.cp_crash=none \
+    config.disable_atlas=true \
+    wifi.interface=wlan0 \
+    ro.cm.hardware.cabc=/sys/class/mdnie/mdnie/cabc
+
+# Dalvik heap
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=128m
+
+# Disable ADB authentication
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.adb.secure=0 \
+    ro.secure=0
+
+# BG Tuning
+PRODUCT_PROPERTY_OVERRIDES += \
+ ro.sys.fw.bg_apps_limit=8 \
+ro.config.low_ram=true \
+persist.sys.force_highendgfx=true
